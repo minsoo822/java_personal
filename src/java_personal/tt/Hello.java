7c6b782c20af4b1a1066ee93ea360c7b37495997
minsoo822@naver.com <minsoo822@naver.com>
2022-08-24 17:10:33 +0900

    java

--- src/java_personal/tt/Hello.java
+++ src/java_personal/tt/Hello.java
@@ -3,7 +3,7 @@ package java_personal.tt;
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Hello world!!");
 		System.out.println(1+1);
 	
